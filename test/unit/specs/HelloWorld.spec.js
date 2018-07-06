d802e3aeaea9aa833203e4d316af54994fa5cd32
IvanSotelo <ivan-.-sotelo@hotmail.com>
2018-07-06 12:40:41 -0600

    Fix Tests

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,13 @@
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld';
+import Vue from 'vue'
+import Home from '@/components/Home'
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App');
-  });
-});
+    const Constructor = Vue.extend(Home)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('.page-home__slogan').textContent)
+      .toEqual('For the 200ᵀᴴ Anniversary')
+    expect(vm.$el.querySelector('.page-home__title').textContent)
+      .toEqual('Ivan Aivazovsky')
+  })
+})
