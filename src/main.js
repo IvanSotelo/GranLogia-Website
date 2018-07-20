36de8dc7075f1d73b998962bfa84d2fbdc96d90d
Ivan Sotelo <ivan-.-sotelo@hotmail.com>
2018-07-19 23:35:21 -0600

    i18n init

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import i18n from './lang'
 
 Vue.config.productionTip = false
 
@@ -12,6 +13,7 @@ new Vue({
   el: '#app',
   router,
   store,
+  i18n,
   components: { App },
   template: '<App/>'
 })
