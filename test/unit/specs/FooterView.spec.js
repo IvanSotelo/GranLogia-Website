158095823ca94044fee1fc9e805fb0057d413fa2
IvanSotelo <ivan-.-sotelo@hotmail.com>
2018-07-25 13:41:22 -0600

    Footer test unit :candy:

--- test/unit/specs/FooterView.spec.js
+++ test/unit/specs/FooterView.spec.js
@@ -1,11 +1,20 @@
-import Vue from 'vue'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import FooterView from '@/components/FooterView'
+import i18n from '../../../src/lang'
+import VueRouter from 'vue-router'
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter()
 
 describe('FooterView.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(FooterView)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.footer__archive').textContent)
-      .toEqual('Archivo')
+    const wrapper = shallowMount(FooterView, {
+      i18n,
+      router,
+      stubs: ['router-link']
+    })
+    expect(wrapper.find('.footer__archive').text()).toBe('Archive')
+    expect(wrapper.find('.footer-nav').text()).toBe('Grand LondgeContact')
   })
 })
