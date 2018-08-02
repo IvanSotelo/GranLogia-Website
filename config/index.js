5ce7b0b15a3623081385f8b3f43cf07b25a0a2d2
IvanSotelo <ivan-.-sotelo@hotmail.com>
2018-08-02 09:39:37 -0600

    erro

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'Paquime-Website',
+    assetsPublicPath: '/Paquime-Website',
 
     /**
      * Source Maps
