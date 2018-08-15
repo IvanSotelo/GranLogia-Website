3d62db71c5bfdd7163d8c3cb2f151bf22f403289
IvanSotelo <ivan-.-sotelo@hotmail.com>
2018-08-15 10:24:45 -0600

    SplitText effect :sunglasses:

--- src/components/Freemasonry.vue
+++ src/components/Freemasonry.vue
@@ -5,12 +5,14 @@ main(role="main")
       .page-freemasonry__background
       .vertical-align
         .vertical-align__item
-          p
+          p.text {{ text }}
 </template>
 
 <script>
-import {TweenLite, Expo} from 'gsap'
+import {TweenLite, Expo, TimelineMax} from 'gsap'
 import { mapGetters, mapActions } from 'vuex'
+import classes from 'dom-classes'
+import SplitText from '../pluggins/SplitText'
 
 export default {
   name: 'Freemasonry',
@@ -19,17 +21,43 @@ export default {
   },
   data () {
     return {
+      split: null,
       text: 'July 29th is the 200th anniversary of the birth of Ivan Aivazvsky. He early showed a special attraction to the sea and was the most brilliant consistent representative seascape painter throughout the XIX century. Aivazovskys creativity deep roots connected with Russian Art of the late XVIII and XIX centuries, and in turn impacted on him fruitfully. He was keenly aware of the charm of the changing sea elements and diversity, this greatly reflected his work. He studied the sea, he kenw it by heart, and with extraordinary ease, he portrayed it.'
     }
   },
   mounted () {
+    this.splitText()
     this.changeRoute(this.$route.name)
   },
   methods: {
     ...mapActions({
       changeRoute: 'ROUTE_NAME'
     }),
+    splitText () {
+      this.split = new SplitText(
+        this.$el.querySelector('.text'),
+        {type: 'words, chars', wordsClass: 'word'}
+      )
+      this.split.words.forEach((t) => {
+        return classes.add(t, 'is-hidden')
+      })
+    },
+    introAnimation () {
+      const n = new TimelineMax({})
+      if (this.isMobile) {
+        n.to(this.$el.querySelector('.text'), 1, {
+          autoAlpha: 1,
+          delay: 1,
+          ease: Expo.easeInOut
+        })
+      } else {
+        n.staggerTo(this.split.words, 0.1, {
+          className: '-=is-hidden'
+        }, 0.1)
+      }
+    },
     animateIn (el, done) {
+      let t = this
       TweenLite.to(this.$el.querySelector('.page'), 1, {
         autoAlpha: 1,
         ease: Expo.easeOut,
@@ -39,7 +67,10 @@ export default {
         autoAlpha: 1,
         ease: Expo.easeInOut,
         delay: 1,
-        onComplete: done
+        onComplete: function () {
+          t.introAnimation()
+          done()
+        }
       })
     },
     leave: function (el, done) {
@@ -55,7 +86,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
+<style lang="scss">
 .page-freemasonry{
   .page-freemasonry__background {
       position: absolute;
@@ -94,23 +125,17 @@ export default {
       font-family: "Playfair Display";
       line-height: 1.4;
       color: #69697d;
-      @media only screen and (max-width:768px) {
-        width: 85vw;
-        color: #85859e;
-        font-size: 1em;
-        opacity: 0
-      }
       .word {
         opacity: 1;
         filter: blur(0);
         transform: translate3d(0, 0, 0);
-        transition: opacity .3s ease-out, filter .3s ease-out, transform .3s ease-out
+        transition: opacity .3s ease-out, filter .3s ease-out, transform .3s ease-out;
       }
-      .word.is-hidden {
-          opacity: 0;
-          filter: blur(5px);
-          transform: translate3d(-10px, 0, 0);
-          transition-duration: 0s
+      .is-hidden {
+        opacity: 0;
+        filter: blur(5px);
+        transform: translate3d(-10px, 0, 0);
+        transition-duration: 0s;
       }
       .word:nth-of-type(1),
       .word:nth-of-type(11),
@@ -123,7 +148,13 @@ export default {
       .word:nth-of-type(42) .word:nth-of-type(67),
       .word:nth-of-type(5),
       .word:nth-of-type(87) {
-          color: #fff
+        color: #fff;
+      }
+      @media only screen and (max-width:768px) {
+        width: 85vw;
+        color: #85859e;
+        font-size: 1em;
+        opacity: 0
       }
   }
 }
