72b108943edf0d1320c5864fb3fec260960bfb85
IvanSotelo <ivan-.-sotelo@hotmail.com>
2018-08-22 11:39:55 -0600

    Home test :fire:

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -1,13 +1,50 @@
-import Vue from 'vue'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Home from '@/components/Home'
+import i18n from '../../../src/lang'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import Config from '../../../src/store/modules/Config'
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+localVue.use(Vuex)
+
+const router = new VueRouter()
 
 describe('Home.vue', () => {
+  let actions
+  let state
+  let store
+
+  beforeEach(() => {
+    state = {
+      Config: {
+        isMobile: false,
+        audio: 'on',
+        routeName: 'is-home'
+      }
+    }
+
+    actions = {
+      ROUTE_NAME: jest.fn()
+    }
+
+    store = new Vuex.Store({
+      state,
+      actions,
+      getters: Config.getters
+    })
+  })
+
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Home)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.page-home__slogan').textContent)
-      .toEqual('For the 200ᵀᴴ Anniversary')
-    expect(vm.$el.querySelector('.page-home__title').textContent)
-      .toEqual('Ivan Aivazovsky')
+    const wrapper = shallowMount(Home, {
+      i18n,
+      localVue,
+      store,
+      router,
+      stubs: ['router-link']
+    })
+    expect(wrapper.find('.page-home__slogan').text()).toBe('For the 200ᵀᴴ Anniversary')
+    expect(wrapper.find('.page-home__title').text()).toBe('Ivan Aivazovsky')
   })
 })
